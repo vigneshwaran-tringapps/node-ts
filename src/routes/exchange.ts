6e57a2589e90157c5f40e991033d23b5263fc057
vigneshwaran26 <vicky.ravi26@gmail.com>
2021-10-25 11:48:15 +0530

    Endpoint query parameter

--- src/routes/exchange.ts
+++ src/routes/exchange.ts
@@ -2,96 +2,112 @@
 import express, {NextFunction, Request,Response} from 'express';
 const router = express.Router();
 
-router.get('/', (request:Request,response:Response,next:NextFunction) => {
 
+router.get('/', (request:Request,response:Response,next:NextFunction) => {
     try{
-        return response.status(200).send(
-            [
-                {
-                  "base":"INR",  
-                  "rate": {
-                      "CAD": 0.017,
-                      "EUR": 0.012,
-                      "GBP": 0.0098,
-                      "INR": 1,
-                      "USD": 0.0130
-                    },
+        const rates:any =  [
+            {
+                "base":"INR",  
+                "rate": {
+                    "CAD": 0.017,
+                    "EUR": 0.012,
+                    "GBP": 0.0098,
+                    "INR": 1,
+                    "USD": 0.0130
+                } ,
                 "symbols": {
-                       "CAD": "Canadian dollar",
-                       "USD":"United states dollar",
-                       "GBP":"great britan pound",
-                       "INR":"Indian rupees",
-                       "EUR":"Euro"
-                    }
-                },
-                {
-                    "base":"USD",  
-                    "rate": {
-                        "INR": 75.07,
-                        "GBP": 0.74,
-                        "CAD": 1.26,
-                        "EUR": 0.87,
-                    },
-                    "symbols": {
-                        "CAD": "Canadian dollar",
-                        "USD":"United states dollar",
-                        "GBP":"great britan pound",
-                        "INR":"Indian rupees",
-                        "EUR":"Euro"
-                     } 
-                },
-                {
-                    "base":"GBP",  
-                    "rate": {
-                        "USD": 1.36,
-                        "INR": 102.10,
-                        "CAD": 1.71,
-                        "EUR": 1.18,
-                    },
-                    "symbols": {
-                        "CAD": "Canadian dollar",
-                        "USD":"United states dollar",
-                        "GBP":"great britan pound",
-                        "INR":"Indian rupees",
-                        "EUR":"Euro"
-                     } 
-                },
-                {
-                    "base":"CAD",  
-                    "rate": {
-                        "USD": 0.80,
-                        "GBP": 0.59,
-                        "INR": 59.75,
-                        "EUR": 0.69,
-                    } ,
-                    "symbols": {
+                    "CAD": "Canadian dollar",
+                    "USD":"United states dollar",
+                    "GBP":"great britan pound",
+                    "INR":"Indian rupees",
+                    "EUR":"Euro"
+                 }
+              },
+              {
+                  "base":"USD",  
+                  "rate": {
+                      "CAD": 1.26,
+                      "EUR": 0.87,
+                      "GBP": 0.74,
+                      "INR": 75.07,
+                      "USD": 1
+                      } ,
+                      "symbols": {
                         "CAD": "Canadian dollar",
                         "USD":"United states dollar",
                         "GBP":"great britan pound",
                         "INR":"Indian rupees",
                         "EUR":"Euro"
                      }
-                },
-                {
-                    "base":"EUR",  
-                    "rate": {
-                        "USD": 1.15,
-                        "GBP": 0.85,
-                        "CAD": 1.45,
-                        "INR": 86.61,
-                    } ,
-                    "symbols": {
-                        "CAD": "Canadian dollar",
-                        "USD":"United states dollar",
-                        "GBP":"great britan pound",
-                        "INR":"Indian rupees",
-                        "EUR":"Euro"
-                     }
-                }
-            ]
+              },
+              {
+                  "base":"GBP",  
+                  "rate": {
+                      "CAD": 1.71,
+                      "EUR": 1.18,
+                      "GBP": 1,
+                      "INR": 102.10,
+                      "USD": 1.36
+                  } ,
+                  "symbols": {
+                    "CAD": "Canadian dollar",
+                    "USD":"United states dollar",
+                    "GBP":"great britan pound",
+                    "INR":"Indian rupees",
+                    "EUR":"Euro"
+                 }
+              },
+              {
+                  "base":"CAD",  
+                  "rate": {
+                      "CAD":1,
+                      "EUR": 0.69,
+                      "GBP": 0.59,
+                      "INR": 59.75,
+                      "USD": 0.80,
+                  } ,
+                  "symbols": {
+                    "CAD": "Canadian dollar",
+                    "USD":"United states dollar",
+                    "GBP":"great britan pound",
+                    "INR":"Indian rupees",
+                    "EUR":"Euro"
+                 }
+              },
+              {
+                  "base":"EUR",  
+                  "rate": {
+                      "CAD": 1.45,
+                      "EUR": 1,
+                      "GBP": 0.85,
+                      "INR": 86.61,
+                      "USD": 1.15,
+                  },
+                  "symbols": {
+                    "CAD": "Canadian dollar",
+                    "USD":"United states dollar",
+                    "GBP":"great britan pound",
+                    "INR":"Indian rupees",
+                    "EUR":"Euro"
+                 } 
+              }
+        ]
+        let rate:any = rates;
+        if(request.query.base && request.query.symbols){
+            rate = rates.find((ele:any)=> ele.base===request.query.base && ele.symbols===request.query.symbols)
+            
+        } else if(Object(request.query).hasOwnProperty("base") || Object(request.query).hasOwnProperty("symbols")){
+            console.log("else if called ::", request.query);
+            rate= rate.find((ele:any)=> ele[Object.keys(request.query)[0]]===request.query[Object.keys(request.query)[0]])
+        }
+
+        return response.status(200).send(
+            rate        
         )
     }catch(error){
       return response.status(500).send(`Somthing went wrong...`);
+    //  next(error)
     }
-   })
+   
+})
 module.exports = router;
